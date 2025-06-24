4338058d2aff41d8d9310b0aa4504934ff37bb48
Anuj Gupta <ganuj2674@gmail.com>
2025-06-24 18:37:16 +0530

    Formatting change

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+
 //Protected Routes
 router.get("/me", authMiddleware, authController.getUser);
 router.post("/onboarding", authMiddleware, authController.onboarding);
