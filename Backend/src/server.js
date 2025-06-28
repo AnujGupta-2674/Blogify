425513c4ae4821671455ae8109877cb8b4b7e544
Anuj Gupta <ganuj2674@gmail.com>
2025-06-29 01:00:32 +0530

    Final Commit and Deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        origin: process.env.CLIENT_URL || 'https://blogify-d0ba.onrender.com/',
         credentials: true, // Allow credentials (cookies, authorization headers, etc.
     }
 ));
