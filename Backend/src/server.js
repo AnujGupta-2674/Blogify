6f2943cfa01a125e9027f4660fb94ad941db84a9
Anuj Gupta <ganuj2674@gmail.com>
2025-09-25 20:36:47 +0530

    Small fixes and improvements

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -24,6 +24,7 @@ app.use(cors({
     credentials: true
 }));
 
+console.log(process.env.GEMINI_API_key);
 const ai = new GoogleGenAI(process.env.GEMINI_API_key);
 
 export const generateBlogWithGenAI = async (prompt) => {
